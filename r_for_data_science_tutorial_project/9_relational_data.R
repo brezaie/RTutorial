5677de4598a5633a577695987c70e190e98be321
Behzad <behzadrezaie11@gmail.com>
2019-07-17 11:07:17 +0430

    Finished relational_data

--- r_for_data_science_tutorial_project/9_relational_data.R
+++ r_for_data_science_tutorial_project/9_relational_data.R
@@ -23,3 +23,49 @@ joined_flights_airlines <- flights %>%
 View(joined_flights_airlines)
 
 ## End of page 186
+library(ggplot2)
+install.packages('maps')
+library(maps)
+airports %>% 
+  semi_join(flights, by = c("faa" = "dest")) %>%
+  ggplot(aes(lon, lat)) + 
+  borders("state") +
+  geom_point() + 
+  coord_quickmap()
+  
+## Get the flights to the top 10 destinations
+top_dest <- flights %>%
+  count(dest, sort = TRUE) %>%
+  head(10)
+flights %>% 
+  filter(dest %in% top_dest$dest)
+
+## To get only the rows in x (which is joined with y), use 'semi_join
+## The above query can run in this way:
+flights %>%
+  semi_join(top_dest)
+
+## Other useful table functions:
+## anti_join(x, y): keeps the rows that have no match with y
+## intersect(x, y): returns only observations both in x and y
+## union(x, y): Return unique observations in x and y
+## setdiff(x, y): Return observations in x, but not in y
+## Examples:
+library(tibble)
+df1 <- tribble(
+  ~x, ~y,
+  1,   1,
+  2,   1
+)
+df2 <- tribble(
+  ~x, ~y,
+  1,   1,
+  1,   2
+)
+intersect(df1, df2)
+union(df1, df2)
+setdiff(df1, df2)
+setdiff(df2, df1)
+
+
+
