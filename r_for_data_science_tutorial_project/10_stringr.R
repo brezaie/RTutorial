5f5e4a6fb1baeda77c46d320a625af070a8714b9
Behzad <behzadrezaie11@gmail.com>
2019-07-17 16:25:35 +0430

    Created stringr

---
+++ r_for_data_science_tutorial_project/10_stringr.R
@@ -0,0 +1,34 @@
+install.packages('stringr')
+library(stringr)
+
+## IMPORTANT: To show the " or ' marks, use 'writeLines' function
+st1 <- "This is the first string \" that we have"
+writeLines(st1)
+
+## str_lenth: get the length of a string
+str_length(c("a", "hello", "goodbye"))
+
+## concat or combine strings
+str_c("hello,", "world", sep = " ")
+x <- c("abd", NA, "'")
+str_c("{", str_replace_na(x), "}")
+
+## conditions in concat
+name <- "Behzad"
+time_of_day <- "morning"
+birthday <- FALSE
+str_c("Good ", time_of_day, " ", name,
+      if(birthday) " and HaPPY BIRTHDAY B-)", ".")
+
+## convert a list of string to a single string
+str_c(c("a", "b", "c"), collapse = " ")
+
+## get the substring
+x <- c("Apple", "Banana", "Pear")
+str_sub(x, 1, 2)
+str_sub(x, 1, 1) <- str_to_lower(str_sub(x, 1, 1))
+
+## sort strings
+x <- c("apple", "eggplant", "banana")
+str_sort(x, locale = "fa")
+
