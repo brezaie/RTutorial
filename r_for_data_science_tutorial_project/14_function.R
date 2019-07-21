ccf4d500f291f325389e154ddd88c9c9a59b4718
Behzad <behzadrezaie11@gmail.com>
2019-07-21 08:50:51 +0430

    Finished function.R

--- r_for_data_science_tutorial_project/14_function.R
+++ r_for_data_science_tutorial_project/14_function.R
@@ -37,7 +37,67 @@ has_name <- function(x) {
 }
 has_name('apple')
 
-calculate_temp <- fucntion(temp) {
+calculate_temp <- function(temp){
+  if(temp <= 0){
     "freezing"
+  }
+  else if(temp > 0 && temp <= 5){
+    "cold"
+  }
+  else if (temp > 5 && temp <= 15){
+    "cool"
+  }
+  else
+  {
+    "dunno"
+  }
+    
 }
+calculate_temp(5)
+
+
+# Check eceptions ---------------------------------------------------------
+
+weighted_mean <- function(x, w)
+{
+  if(length(x) != length(w))
+    stop("`x` and `w` must be the same length", call. = FALSE)
+  
+  sum(w * x) / sum(x)
+}
+## OR
+
+weighted_mean <- function(x, w)
+{
+  stopifnot(length(x) == length(w))
+  
+  sum(w * x) / sum(x)
+}
+weighted.mean(x = c(1:5), w = c(1:2))
+
+
+# Return values -----------------------------------------------------------
+
+complicated_function <- function(x, y, z){
+  if(length(x) == 0 || length(y) == 0)
+    return(0)
+}
+complicated_function(NULL, NULL, NULL)
+
+## When the function draws a plot or prints something, it's better
+## to return the first argument with 'invisible()'
+## This way, the function can be used in pipelines
+show_missings <- function(df) {
+  n <- sum(is.na(df))
+  cat("Missing values: ", n, "\n", sep = "")
+  invisible(df)
+}
+show_missings(mtcars)
+x <- show_missings(mtcars)
+dim(x)
+mtcars %>%
+  show_missings() %>%
+  mutate(mpg = ifelse(mpg < 20, NA, mpg)) %>%
+  show_missings()
+
 
