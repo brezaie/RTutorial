3b3aa6d845346f26dfc73a3bfd4ce9b800ada11a
Behzad <behzadrezaie11@gmail.com>
2019-07-21 11:55:18 +0430

    Update iteration.R

---
+++ r_for_data_science_tutorial_project/16_iteration.R
@@ -0,0 +1,33 @@
+
+# For loop ----------------------------------------------------------------
+
+df <- tibble(
+  a = rnorm(10),
+  b = rnorm(10), 
+  c = rnorm(10),
+  d = rnorm(10)
+)
+
+## Get the median of each column:
+output <- vector("double", ncol(df))
+
+for(i in seq_along(df)){
+  output[[i]] <- median(df[[i]])
+}
+output
+
+
+# Pass a function to another function -------------------------------------
+
+col_summary <- function(df, fun) {
+  out <- vector("double", length(df))
+  for (i in seq_along(df)) {
+    out[[i]] <- fun(df[[i]])
+  }
+  out
+}
+
+col_summary(df, median)
+col_summary(df, mean)
+
+## Page 325
