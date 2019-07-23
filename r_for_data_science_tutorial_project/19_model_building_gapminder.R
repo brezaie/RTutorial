4990d9e0f29f3deaac7079b932da0060d6036f7a
Behzad <behzadrezaie11@gmail.com>
2019-07-23 17:56:59 +0430

    Update grapminder

--- r_for_data_science_tutorial_project/19_model_building_gapminder.R
+++ r_for_data_science_tutorial_project/19_model_building_gapminder.R
@@ -23,4 +23,20 @@ country_model <- function(df) {
 by_country <- by_country %>%
   mutate(model = map(data, country_model))
 
+by_country <- by_country %>%
+  mutate(resids = map2(data, model, add_residuals))
+by_country
 
+resids <- unnest(by_country, resids)
+resids %>% 
+  ggplot(aes(year, resid)) + 
+  geom_line(aes(group = country), alpha = 1 / 3) + 
+  geom_smooth(se = FALSE)
+
+resids %>% 
+  ggplot(aes(year, resid)) + 
+  geom_line(aes(group = country), alpha = 1/3) + 
+  geom_smooth(se = FALSE) + 
+  facet_wrap(~ continent)
+
+## Page 406 - Model Quality
