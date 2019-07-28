4415bb5bdbbe64d578773f5d8d63818bccd0507e
Behzad <behzadrezaie11@gmail.com>
2019-07-28 15:36:42 +0430

    Added working with column-lists

--- r_for_data_science_tutorial_project/19_model_building_gapminder.R
+++ r_for_data_science_tutorial_project/19_model_building_gapminder.R
@@ -1,7 +1,11 @@
-install.packages('gapminder')
+# install.packages('gapminder')
 library(gapminder)
 library(purrr)
 library(tidyr)
+library(ggplot2)
+library(dplyr)
+library(modelr)
+library(tibble)
 gapminder
 
 
@@ -40,11 +44,71 @@ resids %>%
   geom_smooth(se = FALSE) + 
   facet_wrap(~ continent)
 
-## Page 406 - Model Quality
+## Page 406 - Model Quality - Get other model measures 
+##       other than residuals
 library(broom)
 
 glance <- by_country %>%
   mutate(glance = map(model, broom::glance)) %>%
   unnest(glance, .drop = TRUE)
+glance
+
+# glance %>% unnest(resids, .drop = TRUE)
+# glance %>% unnest(glance, .drop = TRUE)
 
 glance %>% arrange(r.squared)
+
+glance %>%
+  ggplot(aes(continent, r.squared)) + 
+  geom_jitter(width = 0.5)
+
+bad_fit <- glance %>% filter(r.squared < 0.25)
+
+# gapminder %>%
+#   semi_join(bad_fit, by = "country")
+
+gapminder %>%
+  semi_join(bad_fit, by = "country") %>%
+  ggplot(aes(year, lifeExp, color = country)) + 
+  geom_line()
+
+
+# How to create column lists? ----------------------------------------------
+
+
+# 1. With nesting ---------------------------------------------------------
+
+gapminder %>% 
+  group_by(country, continent) %>%
+  nest()
+## OR
+gapminder %>%
+  nest(year: gdpPercap)
+
+
+# 2. From vectorized functions ------------------------------------------------
+
+df <- tribble(
+  ~x,
+  "a,b,c",
+  "d,e,f,g"
+)
+
+df %>%
+  mutate(x2 = stringr::str_split(x, ","))
+
+df %>%
+  mutate(x2 = stringr::str_split(x, ",")) %>%
+  unnest()
+
+
+# 3. From a named list -------------------------------------------
+
+x <- list(
+  x = 1:5,
+  b = 3:4,
+  c = 5:6
+)
+
+df <- enframe(x)
+df
